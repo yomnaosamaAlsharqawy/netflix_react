ba2ee748838a1fbf9b1dea2ac32e07ecee10a2e4
kareemzak <kareemahmed96@outlook.com>
2021-04-09 20:26:26 +0200

    added profile login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import LoginForm from './components/forms/LoginForm';
+import ProfileLoginForm from './components/forms/ProfileLoginForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
     // <App />
-    <LoginForm /> 
+    // <LoginForm /> 
+    <ProfileLoginForm />
 ,document.getElementById('root')
 );
 
