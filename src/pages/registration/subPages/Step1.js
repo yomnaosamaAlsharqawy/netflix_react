4c0e0170c4af85161c47ab31065de8028204881b
Adel Kafouri <adelkafouri50@gmail.com>
2021-04-12 05:49:19 +0200

    added step 1 and step 2 in registration

--- src/pages/registration/subPages/Step1.js
+++ src/pages/registration/subPages/Step1.js
@@ -1,7 +1,20 @@
+import './Step1.css'
+
 export default function Step1() {
     return (
-        <div>
-            <h1>Step 1</h1>
+        <div className='SetOneContainer'>
+            <div className="step">
+                STEP <span>1</span> OF <span>3</span>
+            </div>
+            <div className="StepOneHeader">
+                Finish setting up your account.
+            </div>
+            <div className="StepOneContent">
+                <p>Netflix is personalized for you. Create a password to watch Netflix on any device at any time.</p>
+            </div>
+            <div className="StepOneButton">
+                <p>Continue</p>
+            </div>
         </div>
     )
 }
